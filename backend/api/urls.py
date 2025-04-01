793b3ecc0ce1b8b83b7a1479e80136aae5b8779b
edwardpriyatna <edwardpriyatnax@gmail.com>
2025-04-01 13:35:58 +0800

    starting again

--- backend/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("notes/", views.NoteListCreate.as_view(), name="note-list"),
-    path("notes/delete/<int:pk>/", views.NoteDelete.as_view(), name="delete-note"),
-]
-#for each url we got from backend.urls if we find a match, we will handle it and pass it to views
